2e05b57f1cbf1bc4b51906a82856a9ee29a3cd47
hanifaabdullah <153917030+hanifaabdullah@users.noreply.github.com>
2024-02-07 04:37:21 -0800

    Add files via upload

---
+++ 001-area_calculator/bin/circle_arae_calculator.dart
@@ -0,0 +1,5 @@
+import 'package:circle_arae_calculator/circle_arae_calculator.dart' as circle_arae_calculator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${circle_arae_calculator.calculate()}!');
+}
---
+++ 001-area_calculator/test/circle_arae_calculator_test.dart
@@ -0,0 +1,8 @@
+import 'package:circle_arae_calculator/circle_arae_calculator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
